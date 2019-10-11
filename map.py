2e7df420349be71177cc0c39442675aaa591b35e
Unknown <eugene.choy.2016@sis.smu.edu.sg>
2019-10-11 13:53:16 +0800

    Add Marker to Map based on CSV input
    Add Marker to Map based on CSV input

--- map.py
+++ map.py
@@ -10,34 +10,71 @@ mapscreen = Blueprint('map', __name__)
 def map():
     #return "Complete"
     csv_file = open('docs/SampleMsgwLocation.csv')
-    get_coords_with_location(csv_file)
-    return render_template('map.html',title='Map')
+    response = get_coords_with_location(csv_file)
+    print(response)
+    return render_template('map.html',title='Map',jsonResponse=response)
 
 
+@mapscreen.route("/")
 def get_coords_with_location(csv_file):
     csv_reader = csv.reader(csv_file,delimiter=',')
-    line_count = 0
 
     not_found = []
+    successfulFound = []
+
+    venueDict = {}
 
     one_map_api_url = "https://developers.onemap.sg/commonapi/search?returnGeom=Y&getAddrDetails=Y&searchVal="
+
+    next(csv_reader) #skip first line
 
     for row in csv_reader:
         #Get all the row data for this row
         location = row[0]
-        print(location)
         message = row[1]
         sentiment = row[2]
         sentiment_score = row[3]
         date = row[4]
 
-        rowOneMapSearch = one_map_api_url + location
-        response =  requests.get(rowOneMapSearch)
-        results  =  json.loads(response.content.decode('utf-8'))
+        if location not in venueDict.keys():
+            rowOneMapSearch = one_map_api_url + location
+            response =  requests.get(rowOneMapSearch)
+            results  =  json.loads(response.content.decode('utf-8'))
 
-        if(results['found'] !=0):
-            lat = float(results["results"][0]['LATITUDE'])
-            lon = float(results["results"][0]['LONGITUDE'])
-        else:
-            not_found.append(location)
 
+            if(results['found'] !=0):
+                lat = float(results["results"][0]['LATITUDE'])
+                lon = float(results["results"][0]['LONGITUDE'])
+                address = results["results"][0]['ADDRESS']
+                foundObj =  {   
+                                "location" : location,
+                                "address" : address,
+                                "messages" : [{
+                                    "sentiment": sentiment,
+                                    "message": message,
+                                    "sentiment_score" : sentiment_score,
+                                    "date": date,
+                                }],
+                                "lat" : lat,
+                                "lon" : lon
+                            }
+                venueDict[location] = foundObj
+            else:
+                not_found.append(location)
+        else: 
+            foundObj = venueDict[location]
+            foundObj["messages"].append({
+                "sentiment": sentiment,
+                "message": message,
+                "sentiment_score" : sentiment_score,
+                "date": date
+            })
+
+    for key in venueDict.keys():
+        successfulFound.append(venueDict[key])
+    searchJsonResponse =    {
+                                "successful": successfulFound,
+                                "unsuccessful" : not_found
+                            }
+    print (json.dumps(searchJsonResponse))
+    return json.dumps(searchJsonResponse)
